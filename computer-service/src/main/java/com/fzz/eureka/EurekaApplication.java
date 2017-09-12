f0632ebaa615f4dce4b6fe466fe39e7f35c8d71a
fangzongzhou <fangzongzhou@thunisoft.com>
2017-09-12 18:39:22 +0800

    开始学习spring cloud

---
+++ computer-service/src/main/java/com/fzz/eureka/EurekaApplication.java
@@ -0,0 +1,14 @@
+package com.fzz.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+}
---
+++ computer-service/src/main/java/com/fzz/eureka/controller/ComputerController.java
@@ -0,0 +1,35 @@
+package com.fzz.eureka.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.ServiceInstance;
+
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
+
+@RestController
+public class ComputerController {
+    private final Logger logger = Logger.getLogger(String.valueOf(getClass()));
+
+    @Qualifier("simpleDiscoveryClient")
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public Integer add(
+            @RequestParam Integer a,
+            @RequestParam Integer b
+    ) {
+        ServiceInstance instance = client.getLocalServiceInstance();
+        Integer r = a + b;
+        logger.info("/add,host " + instance.getHost() + ", service_id:" + instance.getServiceId() + ",result" + r);
+        return r;
+
+    }
+}
