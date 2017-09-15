a5ea625dbc9c0a9f2a118451d46308c298ccee7b
fangzongzhou <fangzongzhou@thunisoft.com>
2017-09-15 10:49:36 +0800

    修复dashboard不展示问题

--- eureka-client/src/main/java/com/fzz/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/fzz/eurekaclient/EurekaClientApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
-@EnableHystrix
+
 @EnableDiscoveryClient
 @SpringBootApplication
 public class EurekaClientApplication {
--- eureka-consumer-ribbon-hystrix/src/main/java/com/fzz/eurekaconsumerribbonhystrix/EurekaConsumerRibbonHystrixApplication.java
+++ eureka-consumer-ribbon-hystrix/src/main/java/com/fzz/eurekaconsumerribbonhystrix/EurekaConsumerRibbonHystrixApplication.java
@@ -6,9 +6,11 @@ import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
+@EnableCircuitBreaker
+@EnableDiscoveryClient
 @SpringCloudApplication
 public class EurekaConsumerRibbonHystrixApplication {
 
