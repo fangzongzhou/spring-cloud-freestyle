9917b45338a395f55d9973651d3d63bc71625436
fangzongzhou <fangzongzhou@thunisoft.com>
2017-09-25 10:59:58 +0800

    日常提交

--- eureka-client/src/main/java/com/fzz/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/fzz/eurekaclient/EurekaClientApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 
+
 @EnableDiscoveryClient
 @SpringBootApplication
 public class EurekaClientApplication {
